5835eb18f148936d3e63ec367ac8355f05d43196
Dis <sr_5@inbox.ru>
2013-05-09 16:46:19 +0700

    Blocks and yields

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -1,14 +1,17 @@
 require_relative "cart"
 require_relative "item"
 
-cart = Cart.new
-cart.add_item(Item.new)
-cart.add_item(Item.new)
+# cart = Cart.new
+# cart.add_item(Item.new)
+# cart.add_item(Item.new)
 
-p cart.items
+# p cart.items
 
-cart.remove_item
+# cart.remove_item
 
-p cart.items
+# p cart.items
 
-cart.validate
+# cart.validate
+
+item = Item.new({ :price => 10, :weight =>100, :name => "Car" })
+item.info { |attr| puts attr}
