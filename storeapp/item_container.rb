63ad98b89f15421c41a0aae0cc2436daf8c487c8
Dis <sr_5@inbox.ru>
2013-06-25 16:48:04 +0700

    callback of modules

--- storeapp/item_container.rb
+++ storeapp/item_container.rb
@@ -1,9 +1,20 @@
 module ItemContainer
 
-  module Manager
+  module ClassMethods
+
+    # expexted that item's price linked on dollar course
+    def min_price
+      100 # connect to the internet and receive currency price
+    end
+
+  end
+
+  module InstanceMethods
 
     def add_item item
-      @items.push item
+      unless item.price< self.class.min_price
+        @items.push item
+      end
     end
 
     # removes last item
@@ -19,14 +30,18 @@ module ItemContainer
       @items.delete_if { |i| i.price.nil? }
     end
 
-  end
-
-  module Info
-
     def count_valid_items
       @items.count { |i| i.price } # count true where item have price
     end
 
+  end
+
+  # this method named callback, its calls every time when class include ItemContainer
+  def self.included(base) # in argument holds class in which putting module
+    base.extend ClassMethods
+    base.class_eval do
+      include InstanceMethods
+    end
   end
 
 end
