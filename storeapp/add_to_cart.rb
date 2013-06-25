483ba12eee3ca5db9f31db2a8e31fb4d7cccb2a5
Dis <sr_5@inbox.ru>
2013-06-25 18:53:10 +0700

    constants

--- storeapp/add_to_cart.rb
+++ storeapp/add_to_cart.rb
@@ -9,5 +9,5 @@ cart.read_from_file
 begin
   cart.save_to_file
 rescue Cart::ItemNotSupported
-  puts "One of your items is virtual, Cart doesn't support saving virtual items yet"
+  puts "One the items your're trying to save is not supported by the Cart. Unsupported class are: #{Cart::UNSUPPORTED_ITEMS}"
 end
