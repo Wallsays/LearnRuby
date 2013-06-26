5e260d999c83783563f80578d707ce41b68297bf
Dis <sr_5@inbox.ru>
2013-06-26 16:13:43 +0700

    DomainSpecificLanguage (DSL)

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -1,6 +1,22 @@
 require_relative "store_application"
 
-StoreApplication.new
+StoreApplication.config do |app|
+
+  app.name        = "My Store"
+  app.environment = :production
+
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+    admin.login = "admin"
+    admin.send_info_emails_on :mondays
+  end
+
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 @items << AntiqueItem.new("car", :price => 101, :weight =>100)
@@ -13,5 +29,5 @@ cart.add_item RealItem.new({ :weight =>100, :price => 151, :name => "car" })
 cart.add_item RealItem.new({ :weight =>100, :price => 121, :name => "kettle" })
 
 method = "all_cars"
-p cart.send(method)
+# p cart.send(method)
 
