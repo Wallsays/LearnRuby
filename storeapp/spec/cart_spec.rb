a0376111299c3a194c10d953a23323a18fbf2de1
Wallsays <derevyankodenis92@gmail.com>
2013-06-27 00:24:16 +0700

    describe and nested tests

--- storeapp/spec/cart_spec.rb
+++ storeapp/spec/cart_spec.rb
@@ -6,14 +6,24 @@ require_relative "../app/item_container"
 require_relative "../app/cart"
 
 describe Cart do
-  
-  it "adds items into the cart" do
-    cart = Cart.new("roman")
-    item1 = Item.new("kettle", price: 200)
-    item2 = Item.new("car"   , price: 200)
-    cart.add_items(item1, item2)
-    cart.items.should include(item1,item2)
+
+  describe "managing items" do
+
+    it "adds items into the cart" do
+      cart = Cart.new("roman")
+      item1 = Item.new("kettle", price: 200)
+      item2 = Item.new("car"   , price: 200)
+      cart.add_items(item1, item2)
+      cart.items.should include(item1,item2)
+    end
+
+  it "removes items from itself"
+
 
   end
+  
+  it "counts items in itself"
+  it "places order using all items were added into the cart"
+  it "clear itself off items after an order is placed"
 
 end
